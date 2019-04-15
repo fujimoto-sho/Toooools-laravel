79014380e77bb39db162324086062f4065f7cafc
fujisho <fujisho344@gmail.com>
2019-04-16 04:04:59 +0900

    fix

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,9 +1,9 @@
 @if ($paginator->hasPages())
-  <div class="pagenation">
+  <div class="pagination">
     <ul class="pagination-list" role="navigation">
         {{-- Previous Page Link --}}
         @unless ($paginator->onFirstPage())
-            <li class="pagenation-item">
+            <li class="pagination-item">
                 <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
             </li>
         @endif
@@ -12,16 +12,16 @@
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <li class="disabled pagenation-item" aria-disabled="true"><span>{{ $element }}</span></li>
+                <li class="disabled pagination-item" aria-disabled="true"><span>{{ $element }}</span></li>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class="active pagenation-item pagination-item-now" aria-current="page"><span>{{ $page }}</span></li>
+                        <li class="active pagination-item pagination-item-now" aria-current="page"><span>{{ $page }}</span></li>
                     @else
-                        <li class="pagenation-item"><a href="{{ $url }}">{{ $page }}</a></li>
+                        <li class="pagination-item"><a href="{{ $url }}">{{ $page }}</a></li>
                     @endif
                 @endforeach
             @endif
@@ -29,7 +29,7 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li class="pagenation-item">
+            <li class="pagination-item">
                 <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
             </li>
         @endif
